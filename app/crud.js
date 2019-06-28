0d82e8d7739e8e33bc541d2e7f42dba7f544d37c
Pedro Diaz <pdiaz.btm@gmail.com>
2019-06-28 19:43:10 -0400

    add sort

--- app/crud.js
+++ app/crud.js
@@ -2,93 +2,100 @@ const express = require('express');
 const util = require('./utils/util');
 module.exports = (Collection) => {
 
-  // ======
-  // Create
-  // ======
-  const create = (req, res) => {
-    const newEntry = req.body;
-    Collection.create(newEntry, (e, newEntry) => {
-      if (e) {
-        console.log(e);
-        return util.errorResponse(res, e);;
-      } else {
-        return util.okResponse(res, 200, newEntry);
-      }
-    });
-  };
+    // ======
+    // Create
+    // ======
+    const create = (req, res) => {
+        const newEntry = req.body;
+        Collection.create(newEntry, (e, newEntry) => {
+            if (e) {
+                console.log(e);
+                return util.errorResponse(res, e);;
+            } else {
+                return util.okResponse(res, 200, newEntry);
+            }
+        });
+    };
 
-  // =========
-  // Read many
-  // =========
-  const readMany = (req, res) => {
-    let query = req.query || {};
+    // =========
+    // Read many
+    // =========
+    const readMany = (req, res) => {
+        let query = req.query || {};
 
-    Collection.find(query, (e, result) => {
-      if (e) {
-        console.log(e.message);
-        return util.errorResponse(res, e);
-      } else {
-        return util.okResponse(res, 200, result);
-      }
-    });
-  };
+        Collection.find(query, (e, result) => {
+            if (e) {
+                console.log(e.message);
+                return util.errorResponse(res, e);
+            } else {
+                return util.okResponse(res, 200, result);
+            }
+        }).sort({created_at: -1});
+    };
 
-  // ========
-  // Read one
-  // ========
-  const readOne = (req, res) => {
-    const { _id } = req.params;
+    // ========
+    // Read one
+    // ========
+    const readOne = (req, res) => {
+        const {_id} = req.params;
 
-    Collection.findById(_id, (e, result) => {
-      if (e) {
+        Collection.findById(_id, (e, result) => {
+            if (e) {
 
-        console.log(e.message);
-        return util.errorResponse(res, e);
-      } else {
-        return util.okResponse(res, 200, result);
-      }
-    });
-  };
+                console.log(e.message);
+                return util.errorResponse(res, e);
+            } else {
+                return util.okResponse(res, 200, result);
+            }
+        });
+    };
 
-  // ======
-  // Update
-  // ======
-  const update = (req, res) => {
-    const changedEntry = req.body;
-    Collection.update({ _id: req.params._id }, { $set: changedEntry }, (e, result) => {
-      if (e)
+    // ======
+    // Update
+    // ======
+    const update = (req, res) => {
+        const changedEntry = req.body;
+        Collection.update({
+            _id: req.params._id
+        }, {
+            $set: changedEntry
+        }, (e, result) => {
+            if (e) 
+                return util.errorResponse(res, e);
+             else 
+                return util.okResponse(res, 200, result);
+            
+        });
+    };
 
-        return util.errorResponse(res, e);
-      else
-        return util.okResponse(res, 200, result);
-    });
-  };
+    // ======
+    // Remove
+    // ======
+    const remove = (req, res) => {
+        Collection.remove({
+            _id: req.params._id
+        }, (e, result) => {
+            if (e) 
 
-  // ======
-  // Remove
-  // ======
-  const remove = (req, res) => {
-    Collection.remove({ _id: req.params._id }, (e, result) => {
-      if (e)
+                return util.errorResponse(res, e);
+             else 
+                return util.okResponse(res, 200, result);
+            
+        });
+    };
 
-        return util.errorResponse(res, e);
-      else
-        return util.okResponse(res, 200, result);
-    });
-  };
+    // ======
+    // Routes
+    // ======
 
-  // ======
-  // Routes
-  // ======
+    let router = express.Router();
 
-  let router = express.Router();
+    router.post('/', create);
+    router.get('/', readMany);
+    router.get('/:_id', readOne);
+    router.put('/:_id', update);
+    router.delete('/:_id', remove);
 
-  router.post('/', create);
-  router.get('/', readMany);
-  router.get('/:_id', readOne);
-  router.put('/:_id', update);
-  router.delete('/:_id', remove);
-
-  return router;
+    return router;
 
 }
--- app/schemas/document.js
+++ app/schemas/document.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 let documentSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    markdown_text: { type: String}
+    name: {
+        type: String,
+        required: true
+    },
+    markdown_text: {
+        type: String
+    }
 }, {
-        timestamps: {
-            createdAt: 'created_at',
-            updatedAt: 'updated_at'
-        }
-    });
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
 
 module.exports = documentSchema
